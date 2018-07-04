bf961580281c6081e7c8b79e152d28f23e86f066
mrayenm <40813686+mrayenm@users.noreply.github.com>
2018-07-04 09:52:56 +0700

    Add files via upload

---
+++ src/inventori_barang/txtd_dari.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventori_barang;
+
+/**
+ *
+ * @author Rian Maulana
+ */
+class txtd_dari {
+
+    static void setEnabled(boolean x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Object getDate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void requestFocus() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/inventori_barang/txtd_sampai.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventori_barang;
+
+/**
+ *
+ * @author Rian Maulana
+ */
+class txtd_sampai {
+
+    static void setEnabled(boolean x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Object getDate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
